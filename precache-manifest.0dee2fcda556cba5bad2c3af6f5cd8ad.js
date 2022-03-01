64b529f28ed89248b5ed540507856df9c2cf55de
Mickaël <60145073+MickaelMoon@users.noreply.github.com>
2022-03-01 15:10:32 +0100

    Updates

---
+++ precache-manifest.0dee2fcda556cba5bad2c3af6f5cd8ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af6cb5f3c8377c43d2d6",
+    "url": "/blog-react-udemy/static/css/main.6ef419a1.chunk.css"
+  },
+  {
+    "revision": "af6cb5f3c8377c43d2d6",
+    "url": "/blog-react-udemy/static/js/main.af6cb5f3.chunk.js"
+  },
+  {
+    "revision": "3533b32f3f9f09b839b9",
+    "url": "/blog-react-udemy/static/js/1.3533b32f.chunk.js"
+  },
+  {
+    "revision": "3b784f7b88706ecb6456",
+    "url": "/blog-react-udemy/static/js/runtime~main.3b784f7b.js"
+  },
+  {
+    "revision": "c2ec5d54b959e1ecc707d81a7f9584e2",
+    "url": "/blog-react-udemy/index.html"
+  }
+];
